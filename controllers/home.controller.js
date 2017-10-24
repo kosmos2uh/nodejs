88a7e7d6916d232599152b1b28a63da9418c356e
= <kosmos2uh@gmail.com>
2017-10-24 21:34:49 +0300

    lesson 3

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -28,11 +28,11 @@ module.exports = {
 
     const mailOptions = {
       from: `"${req.body.name}<${req.body.email}>"`,
-      to: 'janusnic@gmail.com',
+      to: 'kosmos2uh@gmail.com',
       subject: req.body.subject,
       text: `You have a new submission with the following details... ${req.body.message}`,
       html: `<p>You have a new submission with the following details... ${req.body.message}</p>`,
-    }
+    };
 
     transporter.sendMail(mailOptions, (err, info) => {
       res.redirect('/success');
