8505ce5e422283b3520e70e872eea14147a669d6
= <kosmos2uh@gmail.com>
2017-10-24 21:27:45 +0300

    lesson 3

--- routes/web.js
+++ routes/web.js
@@ -1,31 +1,18 @@
 const router = require('express').Router();
+const controllers = require('../controllers');
 
-// middleware that is specific to this router
-router.use(function timeLog(req, res, next){
-  console.log('Time: ', Date.now());
-  next();
-});
 // define the home page route
-router.get('/', (req, res) => {
-  //res.send('Janus home page');
-  
-  res.render('home', {
-    name: 'Janus',
-    content: '123123'
-  });
-  // res.status = 200;
-  // res.writeHead(200, { 'Content-Type': 'html/plain' });
-});
-// define the about route
-router.get('/about', (req, res) => {
-  res.send('About page');
-});
+router.get('/', controllers.home_controller.index);
 
-router.get('/json', (req, res) => {
-    var json = '{"param1": "value1", "param2": "value2", "param3":{"param4": "value4", "param5": "value5"}}';
-    var fs = require('fs');
-    json = JSON.parse(fs.readFileSync('./public/json.json', 'utf8'));
-    res.render('json', {json: json});
+// define the about route
+router.get('/about', controllers.home_controller.about);
+
+router.get('/contact', controllers.home_controller.contact);
+
+router.post('/send', controllers.home_controller.send);
+
+router.get('/success', (req, res) => {
+  res.render('home/contact/success');
 });
 
 module.exports = router;
